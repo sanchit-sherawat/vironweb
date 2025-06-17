3e31c068f690dcc7cd7656069155f9eb3f13b375
awt-neeraj <neeraj@arihantwebtech.com>
2025-06-17 17:01:57 +0530

    4 Pages Content have been updated with Content formatting & styling by Custom CSS.

--- src/pages/ExpectationsOfViron.js
+++ src/pages/ExpectationsOfViron.js
@@ -3,9 +3,92 @@ import Layout from '../components/Layout';
 function ExpectationsOfViron() {
   return (
     <Layout>
-      <div style={{ padding: '2rem' }}>
-        <h2>Expectations of VIRON</h2>
-        <p>This page is under construction.</p>
+      <div className='container'>
+        <div className="row">
+          <div className='col'>
+            <div style={{ padding: '2rem' }}  className="text-center">
+                <h2 className='text-red'>EXPECTATIONS of VIRON</h2>
+                <p>Last Updated: 17 June 2025</p>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className='col'>
+            <p><strong className='text-blue'>With VIRON, you do not need to attend Meetings, Webinars, nor respond to any third-party MLM Company&rsquo;s "Newsletters". You do not need to contact your Upline (other than to say &ldquo;hi&rdquo;, and you definitely do not need any "training"!</strong><strong> </strong><strong><u>With VIRON&hellip;ALL of that is now irrelevant</u></strong>! *This positive situation is even beneficial for the MLM Company itself, as VIRON handles all the hard work for you to build your MLM downline automatically. <strong><u>Effectively eliminating all the traditional stresses</u></strong>.</p>
+            <hr/>
+            <p>Especially in today&rsquo;s economy, the <strong className='text-red'><u>VIRON Home-Business</u></strong> (&ldquo;<strong className='text-red'><u>VHB</u></strong>&rdquo;) is of utmost value because, whether an individual knows VIRON exists or not, they need or want the VHB, <strong><u>right now</u></strong>, worldwide.&nbsp;</p>
+            <p>*When you directly compare the VHB to the typical business &ldquo;<em>franchise</em>&rdquo; (such as a McDonald&rsquo;s franchise)&hellip;the VHB is arguably <u>superior</u>, because:</p>
+            <ul>
+              <li>Logically, being the most powerful solution, further &ldquo;super-systemized&rdquo; for the network marketing industry, the <strong className='text-blue'><em><u>probability</u></em></strong> of the VHB entering you into a profitable situation in a relatively short time is far higher than any other business (offline or online).</li>
+              <li>When you couple the above with VIRON&rsquo;s <strong className='text-blue'><em><u>affordability</u></em></strong> factor (<strong>the</strong> <strong><u>current market</u> price of a VHB is only $499 total for the total home-business system</strong>), it makes the VHB really the only choice for anyone (whether you are poor or wealthy).</li>
+              <li>Sure, if you are wealthy, you would want multiple VHBs, which is normal. <strong>*But if you are poor or not yet financially free, you really need a VHB! </strong><strong className='text-red'><u>AND CAN AFFORD IT! </u></strong></li>
+              <li><u className='text-blue'>VIRON is naturally civic because it can level the playing field for any individual who wants a VHB</u>.</li>
+            </ul>
+            <p></p>
+            <p className='text-red'><strong><em>Here is a good analogy for understanding VIRON&hellip;</em></strong></p>
+            <p className='text-tab'><strong><em>In the world of Network Marketing, you are expected to go into countless sales &ldquo;battles&rdquo; and do the fighting alone. Wouldn&rsquo;t you like to be able to HIRE &ldquo;BRUCE LEE&rdquo; to do the fighting for you?! </em></strong><br/><span className='text-tab'><strong className='text-red'><em>With VIRON professionals, you now can! </em></strong></span></p>
+          </div>
+        </div>
+        <div className="row">
+          <div className='col'>
+            <div style={{ padding: '2rem' }}  className="text-center">
+                <h2 className='text-blue'>FOR YOUR ADDITIONAL INFORMATION,</h2>
+                <h3 className='text-blue'>Here are the sequential VIRON household expectations:</h3>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <ol>
+              <li>After you have successfully registered with VIRON.NETWORK, you will have access to your Back-Office. <em>After you comfortably review all the information in your back-office, you can fully engage as a paid VIRON Member and enjoy powerful benefits.</em></li>
+              <li>
+                <p>Once you grasp the value of VIRON and its benefits for you, you can engage as follows:</p>
+                <ol type='a'>
+                  <li><strong className='text-blue'>Pay for your Registration.</strong> Currently, during Pre-Launch, there is no cost to register with VIRON.NETWORK.</li>
+                  <li><strong className='text-blue'>Purchase Up-To: 2 Direct Referrals (DRs)</strong>.<br/>
+                    Each DR is priced by demand.<br/><strong>*Current DR pricing is outlined in the back office.</strong>
+                  </li>
+                  <li>The cost of your <strong>VIRON Home-Business (VHB)</strong> includes a position with the qualified<strong> third-party MLM Company</strong>. <em>***To not overload you with too much information here, all the details of said MLM Company, its valuable product, why it has been chosen, and more are showcased within your VIRON back-office. </em></li>
+                  <li>As each of your DR is fulfilled by VIRON - by way of placing a <u>paying</u> member into <u>your</u> Downline (<u>as if you enrolled that person yourself</u>); <strong>You will earn all commissions entitled, including the FAST START BONUS and monthly residuals on that member! </strong></li>
+                </ol>
+              </li>
+            </ol>
+            <ol start="3">
+              <li><strong className='text-blue'>As VIRON <u>continues</u> to fulfill DRs for the people that are placed under you, your commissions and income with the MLM company will grow.</strong> As your income rises (above the break-even point), you will enter profit and rise towards financial freedom. <strong><em>***All details of the Compensation Plan for the MLM Company will also be showcased in your VIRON Back-Office.</em></strong></li>
+              <li><strong className='text-blue'><u>While you enjoy using its incredible products</u></strong> and as VIRON Powers you into Profit, you can <u>optionally</u> share about VIRON with others. This effort will earn you even more income faster, and there is virtually no limit on how much you can earn.</li>
+            </ol>
+          </div>
+        </div>
+        <div className="row">
+          <div className='col'>
+            <div style={{ padding: '2rem' }}  className="text-center">
+                <h2 className='text-blue'>SUMMARY & RECAP:</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className='col'>
+           <p><strong>EMPHASIS</strong>: After you pay and acquire your VHB, your funds are placed into ESCROW with VIRON. Thereafter<span className='text-red'><u>, your funds are </u><strong><em><u>only drawn</u></em></strong><u> at each </u><strong><em><u>completed</u></em></strong><u> step below</u></span>:</p>
+          <ol type="A">
+            <li>Your account with the <strong>third-party MLM company</strong> is paid for and set up. <strong><em>You will then have access to its products and can enjoy the significant benefits.</em></strong></li>
+            <li>Your <strong>DRs</strong> <u>fulfilled</u>. <strong><em>STRONG EMPHASIS: We are spending professional effort and money to generate the DR for you <u>before</u> that DR is even placed into your MLM downline and <u>before</u> we draw your funds!</em></strong><br/><p>Therefore, your success is indeed our success.</p></li>
+          </ol>
+          <p className='text-red'>We are experts in our field, so please be assured that you are in the hands of professionals.</p>
+          <p><strong>VIRON will do all the hard work for you so you can spend more time with your Family. </strong></p>
+          <p>Outside of <strong>VIRON</strong>, a person <u>rarely</u> achieves profit before sharing about an &ldquo;opportunity&rdquo; with others. With <strong>VIRON</strong>, all that changes because we want you to be Profitable even <u>before</u> you decide to share your experience with <strong>VIRON</strong> to others. With <strong className='text-red'><u>&ldquo;Proof Of Success&rdquo;</u></strong>, this will make a significant difference in your desire to expand your business with us, as well as for everyone in your downline.</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className='col'>
+            <div style={{ padding: '2rem' }}  className="text-center">
+              <h2>ALL ACCOUNTS ARE PROCESSED ON A&nbsp; <br/><span className='text-blue'>FIRST-COME-FIRST-SERVE BASIS.</span></h2>
+              <p className='text-blue'><strong>So don&rsquo;t delay.</strong><br/><strong>Start your Turn-Key Home-Business now.</strong></p>
+              <p className='text-red'><strong>Complete your VIRON Home-Business (&ldquo;VHB&rdquo;) now by completing the instructions that are displayed on your back-office when you log-in.</strong></p>
+            </div>
+          </div>
+        </div>
+
       </div>
     </Layout>
   );
--- src/pages/MLMCompanyQualifications.js
+++ src/pages/MLMCompanyQualifications.js
@@ -3,10 +3,58 @@ import Layout from '../components/Layout';
 function MLMCompanyQualifications() {
   return (
     <Layout>
-      <div style={{ padding: '2rem' }}>
-        <h2>MLM Company Qualifications</h2>
-        <p>This page is under construction.</p>
+      <div className='container'>
+        <div className="row">
+          <div className='col'>
+            <div style={{ padding: '2rem' }}  className="text-center">
+                <h2 className="text-blue">Third-Party MLM Company</h2>
+                <h3 className='text-red'><strong><em>Qualification </em></strong><strong>Parameters</strong></h3>
+                <p>Article Updated: 17 June 2025</p>
+            </div>
+            <div>
+              <p>VIRON.NETWORK, aka: "<strong className='text-red'>VIRON</strong>", has non-arbitrarily <em>qualified</em> the third-party MLM Company to be marketed by <strong>VIRON</strong>. It is essential that you understand the VIRON Qualification Parameters apply to any MLM Company that VIRON will build <em><u>for</u></em> you.</p>
+              <p>NOTE: The third-party MLM company is also known as the &ldquo;<em>VEHICLE</em>&rdquo;. The information and details are published in your VIRON Back-Office / Members area.&nbsp;</p>
+              <p>First, the MLM Company&rsquo;s leadership must be <u>ethical</u>. Its compensation plan must be rewarding and <u>civic</u>. <strong><em>*This overall perspective is vital to VIRON&rsquo;s presence and global objective. </em></strong><em>In other words, VIRON represents you; it would be no good if the MLM company makes us &ldquo;look bad&rdquo;.<strong> </strong></em></p>
+              <p></p>
+              <p>Further, the MLM Company&rsquo;s product(s) must be <u>affordable</u>. And they must have <u>strong financial backing and a solid management team</u> for long-term and stable growth. They must have a sound plan to <u>outperform their nearest competitors</u> in their market.</p>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col'><hr /></div>
+        </div>
+        <div className='row'>
+          <div class="col">
+            <p>We strongly look for overall <strong><em><u>SYNERGY</u></em></strong>. Meaning, the MLM Company must be useful value to you and/or able to help you increase your bottom line and/or be a positive element in your Life.</p>
+            <p><strong>Availability:</strong></p>
+            <p>Since inception, VIRON&rsquo;s <strong className='text-red'><em>&ldquo;Let Professionals Build Your Network Marketing Business <u>for</u> You&rdquo;</em></strong> has been substantiated to be of the HIGHEST in DEMAND concept and is of the LOWEST in conversion &ldquo;<em>Resistance</em>&rdquo;. VIRON is <u>internationally ready</u> and literally <u>available</u> ANYWHERE there is an &ldquo;<em>internet connection</em>&rdquo;. Hence, VIRON requires the MLM Company to be international-ready as well (so VIRON systems are <u>able</u> to deliver access and success to its Members worldwide). </p>
+            <p>Example: if VIRON markets an MLM Company that is NOT available in your Country or the &ldquo;<em>product</em>&rdquo; cannot be <em>delivered</em> to you - it makes VIRON &ldquo;<em>unavailable</em>&rdquo; to you as well, and VIRON would not be able to do anything for you in terms of building you the income from that MLM Company.</p>
+            <p>So, we found and concluded that MLM Companies with &ldquo;<em>tangible</em>&rdquo; type of products and not &ldquo;binary&rdquo; or structured un-civically. A vertical matrix naturally inherits the most &ldquo;<em>Spill</em>&rdquo; over factor, which helps people and is = More Civic. <em>*So, a SL Matrix is best, and a &ldquo;forced&rdquo; matrix would be the next best thing. </em></p>
+            <p><strong>Because VIRON Members only deserve the best, if the MLM Company does not meet the above requirements, it cannot be qualified. </strong></p>
+            <hr/>
+            <p><strong>Sustainability:</strong></p>
+            <p>To ensure sustainability, VIRON requires that an Individual can enjoy and benefit from just using the Product of the MLM Company alone (<em>at a fair price</em>) - WITHOUT any &ldquo;<em>Referring</em>&rdquo; or &ldquo;<em>Recruiting</em>&rdquo; or &ldquo;<em>Sponsoring</em>&rdquo;.</p>
+            <p>The &ldquo;Referring&rdquo; is only suggested [if] one wishes to earn more money based on the foundation stated above. <em>*Much like being able to earn a commission for referring someone to a great restaurant. </em></p>
+            <p><strong>Last, but not least.</strong></p>
+            <p>For those of you who have asked or are concerned about: <strong><em>&ldquo;WHAT HAPPENS IF the MLM COMPANY THAT VIRON IS MARKETING GOES OUT OF BUSINESS OR becomes insolvent or is SHUT DOWN BY THE GOVERNMENT&rdquo;?</em></strong> </p>
+            <p>The answer to that is simple:</p>
+            <p>We have designed the VIRON organization to provide protection and maintain an advantage. Meaning, as you engage with VIRON and are Registered (Time &amp; Date Stamped), you are &ldquo;<em>Set for Life</em>&rdquo;. Because this very data is securely transmitted to the MLM Company at the time we engage with them. They will securely use that data to position you <u>correctly</u> and all VIRON Members accordingly. In the rare chance that the MLM</p>
+          </div>
+          <div class="col">
+            <p>naturally inherit &ldquo;<em>delivery</em>&rdquo; or <em>availability</em> problems for our Members. Hence, to ensure that we can touch your life wherever you may be, VIRON will only qualify &ldquo;<em><u>intangible</u></em>&rdquo; type of products that can be delivered internationally (i.e. Software, Digital, Travel Clubs, Savings Clubs, Education/Knowledge, Online Service, etc.).&nbsp;</p>
+            <p><strong>Compatibility:</strong></p>
+            <p>Importantly, the MLM company&rsquo;s CEO and its Administration would need to have a direct corporate relationship with VIRON to accommodate the ongoing <span className='text-red'><em><u>Reassignment</u></em> of VIRON-sponsored or <strong>Direct Referrals</strong> (&ldquo;<strong>DRs</strong>&rdquo;) </span><u>to be placed into your Downline</u>.</p>
+            <p><strong>Compensation Plan:</strong></p>
+            <p>The MLM company&rsquo;s compensation plan must have a high <strong><em><u>achievability</u></em></strong> factor. Statistic shows that the average person in MLM can only &ldquo;Sponsor&rdquo; or &ldquo;Recruit&rdquo; 2 (two) people or less. <span className='text-red'>Although <strong>VIRON</strong> will professionally build your Downline for you and will be doing all the hard work, the easier it is to achieve a certain income level, of course, the better it is for everyone involved.</span> Further, the Compensation Plan is preferred to be a <em><u>Vertical or &ldquo;Straight Line&rdquo; (SL) Matrix</u></em> as well Company &ldquo;goes out of business&rdquo;, it will NOT hurt you or any VIRON Member in any way because we can simply implement the BACK-UP MLM Company &ndash; immediately, by way of sending the same secured (Time &amp; Date Stamped) data to the new MLM Company that is replacing the existing entity, [if] they become insolvent.</p>
+            <p>The only possible cost to you from that situation is to use a portion of your previous profits to pay for the new Registration/Engagement costs with the new company <span className='text-red'>(<em>to ensure an immediate </em><em><u>continuation</u> of your income with no significant disruptions</em>)</span>. This would be considered the &ldquo;worst-case scenario&rdquo; as opposed to <em>&ldquo;having to start all over&rdquo;</em> every time an MLM Company goes out of business or <em>&ldquo;goes bad&rdquo;</em>. This is a remarkable phenomenon when you are part of VIRON, as a unified organization.</p>
+            <p><strong className='text-red'>Also remember, <u>VIRON is spending good money and energies marketing the MLM company, ongoing</u>. </strong>Thus, they need to maintain their license and compliance.</p>
+            <p>Hence, as a Civic company with a Civic purpose, VIRON shall always have the upper hand, and we will not allow any third-party disruptions/corruption/ineptitude to jeopardize the future of any VIRON Member &ndash; period.</p>
+            <p>As we unite and journey together as a ONE-UNIT (TEAM) and Company, your MLM <em>Salvation</em> AND natural <em><u>Defense</u></em> of yours and your Family&rsquo;s Dreams &amp; Reputation are all protected by VIRON.</p>
+            <p><strong><em>-The VIRON Administration</em></strong></p>
+          </div>
+        </div>
       </div>
+      
     </Layout>
   );
 }
