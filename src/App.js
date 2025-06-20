14b6902fabf09a41365fb2ddab11835c6b6cf891
“sanchit-sherawat” <sanchitsherawatk@gmail.com>
2025-06-21 04:23:45 +0530

    Replace HashRouter with BrowserRouter in App.js for improved routing functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
