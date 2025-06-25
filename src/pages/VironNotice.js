37f258b2467ae513821e5f034e8b5909d02cf3cf
awt-neeraj <neeraj@arihantwebtech.com>
2025-06-25 13:58:40 +0530

    Backend Changes done according to client feedback.

--- src/pages/VironNotice.js
+++ src/pages/VironNotice.js
@@ -98,7 +98,6 @@ function VironNotice() {
                         <div style={{ paddingTop: '2rem' }} className="text-center">
                             <img src='https://viron.network/assets/img/viron-logo.svg' alt="Viron Logo" className="img-logo-center" />
                             <h2 className='mtb-15' style={{fontSize:'3em'}}>MEMBER CENTER</h2>
-                            <p className='text-red text-center mb-0'><strong>:::</strong></p>
                             <h3 className='text-red mt-0'><strong><u>CRITICAL NOTICES & INSTRUCTIONS.</u></strong></h3>
                         </div>
                         <div>
@@ -108,21 +107,18 @@ function VironNotice() {
                             <p className='pb-15'><strong><em>&ldquo;People Fail&hellip;Systems Prevail&rdquo;</em></strong>. In the world that we live in, no one can guarantee you perfection. However, we genuinely believe and have utmost confidence <strong>that your VIRON Home-Business can give you the best chance for success.</strong> <u className='text-red'>VIRON is to be by your side long term. For your success, VIRON means serious business. Please read everything carefully to ensure you have the correct expectations.</u></p>
                             <h3 className='text-red text-center mb-0 mt-0'><strong>NOTICE 2: </strong></h3>
                             <p className='text-center pb-25 mt-0'>The qualified, third-party MLM company that is integrated with VIRON systems is: &ldquo;<strong>SAVE CLUB</strong>&rdquo; (<strong>SC</strong>). <em>*Their information and details are located in the left column of this page, under &ldquo;Information &amp; Resources&rdquo;. </em><strong>NOTICE: Although SC has been integrated with VIRON systems, please note that VIRON and SC (a third-party company) are <u>separate entities</u>.</strong></p>
-                            <p className='text-center mb-0'><strong>:::</strong></p>
                             <h3 className='text-red text-center mb-0 mt-0'><strong>NOTICE 3: </strong></h3>
                             <p className='text-red text-center mt-0'>Know your business costs. The itemized cost and details of your <strong><u>VIRON Home-Business (&ldquo;VHB&rdquo;)</u></strong> is as follows:</p>
-                            <ul className='pb-25'>
+                            <ul className='pb-15 mb-0'>
                                 <li><strong className='text-red'><u>SAVE CLUB (SC) Annual Fee: $300</u></strong>. *For your convenience, after you make full payment to VIRON, we will <u>correctly</u> set up your account with SC. Furthermore, to ensure your Turn-Key experience and to ensure you are in the correct position within the VIRON matrix, always allow VIRON professionals to handle your account setup. <strong>It is <u>not</u> recommended for anyone to set-up their accounts directly with SC.</strong> <span className='text-blue'>*EXTREME VALUE: Please note that with this annual payment to SC, you will gain access to the SC product and enjoy its associated savings.</span> <span className='text-highlighted'>NOTE: <u>The <strong>SAVINGS</strong> alone from your daily regular spending can exceed the entire cost of your VIRON Home-Business</u>!</span></li>
                                 <li><strong>VIRON One-Time Registration</strong> <strong>Fee</strong> <del>$39.95</del> (waived during pre-launch).</li>
                                 <li><strong>VIRON Annual Registration Fee</strong> <del>$149.95</del> (waived during pre-launch).</li>
                                 <li><strong>VIRON Direct Referrals (DRs)</strong> $195 each x2 = <del>$390</del> <span className='text-red'><strong>(<u>reduced to only $199 total during this Pre-Launch</u>).</strong></span> <em>*This limited pricing is subject to change without notice and in accordance with market demand. NOTE: There is a limit of 2 DRs per member, because you will only need 2 DRs to qualify for all commissions and bonuses per the SC Compensation Plan. <strong>STRONG NOTE: Each DR will bring forth a subsequent DR that will be placed under them, and so forth. <span className='text-highlighted'><u>Ensuring your downline grows with PAYING members, ongoing!</u></span> </strong></em></li>
                             </ul>
-                            <h2 className='text-red text-tab pb-20'><strong>TOTAL COST DURING PRE-LAUNCH</strong>:<br />Above totals $499.00 + $20 crypto transaction fee = <strong>$<u>519.00</u></strong></h2>
-                            <p className='text-center pb-40'><strong>NOTE: </strong>If the cryptocurrency value drops while we are processing your transaction and setting up your account, it may cause a stall in the process. In this case, you will be contacted to provide the difference. <strong><em>*To avoid the risk of this inconvenience, the &ldquo;crypto transaction fee&rdquo; will also help cover the typical fluctuations in the crypto market for you.</em></strong></p>
+                            <h2 className='text-red text-tab pb-20 mt-0'><strong>TOTAL COST DURING PRE-LAUNCH</strong>:<br />Above totals $499.00 + $20 crypto transaction fee = <strong>$<u>519.00</u></strong></h2>
+                            <p className='text-center text-blue pb-10'><strong>NOTE: </strong>If the cryptocurrency value drops while we are processing your transaction and setting up your account, it may cause a stall in the process. In this case, you will be contacted to provide the difference. <strong><em>*To avoid the risk of this inconvenience, the &ldquo;crypto transaction fee&rdquo; will also help cover the typical fluctuations in the crypto market for you.</em></strong></p>
                             <p className='text-center mb-0'><strong>:::</strong></p>
                             <h3 className='text-red text-center mt-0 paymentArea'><strong>PAYMENT AREA.</strong></h3>
-                            
-                            <p className='text-center mb-0'><strong>:::</strong></p>
                             <p className='text-center pb-20 mt-0 mb-0'>During Pre-Launch, we are <u>manually</u> accepting cryptocurrency payments:<br />Bitcoin (<strong>BTC</strong>),<br />Ethereum (<strong>ETH</strong>),<br /><strong>USDT</strong> (<em>Tron Network</em>).</p>
                             <p className='text-center text-red mt-0 mb-0'>To complete your <strong>VIRON Home-Business</strong> (&ldquo;<strong>VHB</strong>&rdquo;),<br />please send your Cryptocurrency payment to:</p>
                             <hr className='mt-0'/>
@@ -178,8 +174,7 @@ function VironNotice() {
                                 </div>
                             </div>
 
-                            <p><strong className='text-red'>FINAL NOTE: </strong>Once you have completed the above instructions and have sent in your crypto payment, <u>we will notify you accordingly</u>. When your VIRON Home-Business is confirmed, you can relax, sit back, and monitor your business growth as VIRON efforts take back your freedom.</p>
-                            <p className='pb-40'><strong>Congratulations on discovering VIRON.NETWORK!</strong></p>
+                            <p className='pb-40'><strong className='text-red'>FINAL NOTE: </strong>Once you have completed the above instructions and have sent in your crypto payment, <u>we will notify you accordingly</u>. When your VIRON Home-Business is confirmed, you can relax, sit back, and watch your business grow.</p>
                             <p><strong><em>-The VIRON Administration</em></strong></p>
                             <p className='text-center text-blue pt-20'>FYI: *STRIPE payment method, coming soon.</p>
                             <p className='text-center text-blue'>&amp;</p>
