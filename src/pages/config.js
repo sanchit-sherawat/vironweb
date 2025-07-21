f94a33641e0c5890bf384a874da1a8f567e03dac
“sanchit-sherawat” <sanchitsherawatk@gmail.com>
2025-07-21 08:24:02 +0530

    config

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,12 +1,12 @@
-// const API_BASE_URL = 'https://viron.network/api';
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://viron.network/api';
+// const API_BASE_URL = 'http://localhost:3000/api';
 
 
 // export default API_BASE_URL;
 // https://148.113.201.173:4000/edit-account
 
-// const ADMIN_BASE_URL = 'https://viron.network';
-const ADMIN_BASE_URL = 'http://localhost:3000';
+const ADMIN_BASE_URL = 'https://viron.network';
+// const ADMIN_BASE_URL = 'http://localhost:3000';
 
 
 
